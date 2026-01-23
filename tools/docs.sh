fad56aa1565fac3a1c7cf1c20fd3f5c77edb3f55
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-01-23 11:31:25 +0000

    Deploy site 2026-01-23 11:31:18

---
+++ tools/docs.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+function gh_pages()
+{
+  cd docs/_site || return
+  git init
+  git checkout -B gh-pages
+  git add -A
+  git commit -m "Deploy site $(date +'%Y-%m-%d %H:%M:%S')" || echo "No changes to commit"
+  git remote add origin https://github.com/jinghuazhao/jinghuazhao.github.io
+  git push --set-upstream origin gh-pages
+  cd - || return
+}
+
+cd docs
+bundle exec jekyll build
+cd -
+gh_pages
+
+module load gettext/0.21/gcc/qnrcglqo
+module load libiconv/1.16/gcc/4miyzf3w
+
+if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
+   echo icelake
+   module load ceuadmin/libssh/0.10.6-icelake
+   module load ceuadmin/openssh/9.7p1-icelake
+fi
+
+git diff --name-only -z | while IFS= read -r -d '' f; do
+   echo "Processing $f"
+   git add ${f}
+   git commit -m "${f}"
+done
+git push
+du -hs --exclude .git --exclude docs/_site
